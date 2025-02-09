4e5157490664e62d51744726a7e977baee534853
Steve Burg <sburggsx@hotmail.com>
2025-02-09 11:19:27 -0500

    Final

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,5 +1,25 @@
-public class Main {
+import java.util.Scanner;
+
+
+public class ShipCostCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        double itemCost = 0;
+        double shipCost = 0;
+        double SHIPPING_RATE = 0.02;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter the price of the item: ");
+
+        if (in.hasNextDouble()) {
+            itemCost = in.nextDouble();
+            if (itemCost >= 100) {
+                shipCost = 0;
+            }
+            else {
+                shipCost = itemCost * SHIPPING_RATE;
+            }
+        }
+        System.out.print("Your shipping cost will be: $" + shipCost);
     }
 }
